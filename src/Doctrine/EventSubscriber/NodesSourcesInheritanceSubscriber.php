1d7f6bcb66b96ce4b13e8b4d4949c05455423a7e
Ambroise Maupate <ambroise@rezo-zero.com>
2021-11-30 14:14:27 +0100

    Allow indexed node-type fields with single_table inheritance

--- src/Doctrine/EventSubscriber/NodesSourcesInheritanceSubscriber.php
+++ src/Doctrine/EventSubscriber/NodesSourcesInheritanceSubscriber.php
@@ -7,6 +7,7 @@ use Doctrine\Common\EventSubscriber;
 use Doctrine\ORM\Event\LoadClassMetadataEventArgs;
 use Doctrine\ORM\Events;
 use Doctrine\ORM\Mapping\ClassMetadataInfo;
+use RZ\Roadiz\Contracts\NodeType\NodeTypeFieldInterface;
 use RZ\Roadiz\CoreBundle\Bag\NodeTypes;
 use RZ\Roadiz\CoreBundle\DependencyInjection\Configuration;
 use RZ\Roadiz\CoreBundle\Entity\NodesSources;
@@ -15,7 +16,7 @@ use RZ\Roadiz\CoreBundle\Entity\NodeType;
 /**
  * @package RZ\Roadiz\Core\Events
  */
-class NodesSourcesInheritanceSubscriber implements EventSubscriber
+final class NodesSourcesInheritanceSubscriber implements EventSubscriber
 {
     private NodeTypes $nodeTypes;
     private string $inheritanceType;
@@ -29,7 +30,6 @@ class NodesSourcesInheritanceSubscriber implements EventSubscriber
         $this->nodeTypes = $nodeTypes;
         $this->inheritanceType = $inheritanceType;
     }
-
 
     /**
      * @inheritDoc
@@ -62,13 +62,51 @@ class NodesSourcesInheritanceSubscriber implements EventSubscriber
                 }
                 $metadata->setDiscriminatorMap($map);
 
+                /*
+                 * MAKE SURE these parameters are synced with NodesSources.php annotations.
+                 */
+                $nodeSourceTableAnnotation = [
+                    'name' => $metadata->getTableName(),
+                    'indexes' => [
+                        ['columns' => ['discr']],
+                        ['columns' => ['title']],
+                        ['columns' => ['published_at']],
+                        'ns_node_translation_published' => ['columns' => ['node_id', 'translation_id', 'published_at']],
+                        'ns_node_discr_translation' => ['columns' => ['node_id', 'discr', 'translation_id']],
+                        'ns_node_discr_translation_published' => ['columns' => ['node_id', 'discr', 'translation_id', 'published_at']],
+                        'ns_translation_published' => ['columns' => ['translation_id', 'published_at']],
+                        'ns_discr_translation' => ['columns' => ['discr', 'translation_id']],
+                        'ns_discr_translation_published' => ['columns' => ['discr', 'translation_id', 'published_at']],
+                        'ns_title_published' => ['columns' => ['title', 'published_at']],
+                        'ns_title_translation_published' => ['columns' => ['title', 'translation_id', 'published_at']],
+                    ],
+                    'uniqueConstraints' => [
+                        ['columns' => ["node_id", "translation_id"]]
+                    ]
+                ];
+
                 if ($this->inheritanceType === Configuration::INHERITANCE_TYPE_JOINED) {
                     $metadata->setInheritanceType(ClassMetadataInfo::INHERITANCE_TYPE_JOINED);
                 } elseif ($this->inheritanceType === Configuration::INHERITANCE_TYPE_SINGLE_TABLE) {
                     $metadata->setInheritanceType(ClassMetadataInfo::INHERITANCE_TYPE_SINGLE_TABLE);
+                    /*
+                     * If inheritance type is single table, we need to set indexes on parent class: NodesSources
+                     */
+                    foreach ($nodeTypes as $type) {
+                        $indexedFields = $type->getFields()->filter(function (NodeTypeFieldInterface $field) {
+                            return $field->isIndexed();
+                        });
+                        /** @var NodeTypeFieldInterface $indexedField */
+                        foreach ($indexedFields as $indexedField) {
+                            $nodeSourceTableAnnotation['indexes']['nsapp_'.$indexedField->getName()] = [
+                                'columns' => [$indexedField->getName()],
+                            ];
+                        }
+                    }
                 } else {
                     throw new \RuntimeException('Inheritance type not supported: ' . $this->inheritanceType);
                 }
+                $metadata->setPrimaryTable($nodeSourceTableAnnotation);
             } catch (\Exception $e) {
                 /*
                  * Database tables don't exist yet
