6e1fb540800b4a3ac8ae5da840158a2ea91a26c2
Ambroise Maupate <ambroise@rezo-zero.com>
2022-01-12 10:38:44 +0100

    Added cache-tags for WebResource request

--- src/Api/Controller/GetWebResponseByPathController.php
+++ src/Api/Controller/GetWebResponseByPathController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace RZ\Roadiz\CoreBundle\Api\Controller;
 
+use ApiPlatform\Core\Api\IriConverterInterface;
 use ApiPlatform\Core\Exception\InvalidArgumentException;
 use RZ\Roadiz\Core\AbstractEntities\PersistableInterface;
 use RZ\Roadiz\CoreBundle\Api\DataTransformer\WebResponseDataTransformerInterface;
@@ -12,49 +13,63 @@ use RZ\Roadiz\CoreBundle\Entity\Redirection;
 use RZ\Roadiz\CoreBundle\Routing\PathResolverInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 final class GetWebResponseByPathController extends AbstractController
 {
     private RequestStack $requestStack;
     private PathResolverInterface $pathResolver;
     private WebResponseDataTransformerInterface $webResponseDataTransformer;
+    private IriConverterInterface $iriConverter;
 
     /**
      * @param RequestStack $requestStack
      * @param PathResolverInterface $pathResolver
      * @param WebResponseDataTransformerInterface $webResponseDataTransformer
+     * @param IriConverterInterface $iriConverter
      */
     public function __construct(
         RequestStack $requestStack,
         PathResolverInterface $pathResolver,
-        WebResponseDataTransformerInterface $webResponseDataTransformer
+        WebResponseDataTransformerInterface $webResponseDataTransformer,
+        IriConverterInterface $iriConverter
     ) {
         $this->requestStack = $requestStack;
         $this->pathResolver = $pathResolver;
         $this->webResponseDataTransformer = $webResponseDataTransformer;
+        $this->iriConverter = $iriConverter;
     }
 
     public function __invoke(): ?WebResponseInterface
     {
-        if (
-            null === $this->requestStack->getMainRequest() ||
-            empty($this->requestStack->getMainRequest()->query->get('path'))
-        ) {
-            throw new InvalidArgumentException('path query parameter is mandatory');
+        try {
+            if (
+                null === $this->requestStack->getMainRequest() ||
+                empty($this->requestStack->getMainRequest()->query->get('path'))
+            ) {
+                throw new InvalidArgumentException('path query parameter is mandatory');
+            }
+            $resource = $this->normalizeResourcePath(
+                (string) $this->requestStack->getMainRequest()->query->get('path')
+            );
+            $this->requestStack->getMainRequest()->attributes->set('data', $resource);
+            return $this->webResponseDataTransformer->transform($resource, WebResponseInterface::class);
+        } catch (ResourceNotFoundException $exception) {
+            throw new NotFoundHttpException($exception->getMessage(), $exception);
         }
-        $resource = $this->normalizeNodesSourcesPath(
-            (string) $this->requestStack->getMainRequest()->query->get('path')
-        );
-        $this->requestStack->getMainRequest()->attributes->set('data', $resource);
-        return $this->webResponseDataTransformer->transform($resource, WebResponseInterface::class);
     }
 
     /**
      * @param string $path
      * @return PersistableInterface|null
      */
-    protected function normalizeNodesSourcesPath(string $path): ?PersistableInterface
+    protected function normalizeResourcePath(string $path): ?PersistableInterface
     {
+        /*
+         * Serve any PersistableInterface Resource by implementing
+         * your PathResolver and tagging it "roadiz_core.path_resolver"
+         */
         $resourceInfo = $this->pathResolver->resolvePath($path, ['html', 'json'], true);
         $resource = $resourceInfo->getResource();
 
@@ -65,11 +80,23 @@ final class GetWebResponseByPathController extends AbstractController
             $resource instanceof Redirection &&
             null !== $resource->getRedirectNodeSource()
         ) {
-            return $resource->getRedirectNodeSource();
+            $resource = $resource->getRedirectNodeSource();
         }
+
+        $this->addResourceToCacheTags($resource);
+
         /*
          * Or plain entity
          */
         return $resource;
+    }
+
+    protected function addResourceToCacheTags(?PersistableInterface $resource)
+    {
+        $request = $this->requestStack->getMainRequest();
+        if (null !== $request && null !== $resource) {
+            $iri = $this->iriConverter->getIriFromItem($resource);
+            $request->attributes->set('_resources', $request->attributes->get('_resources', []) + [$iri]);
+        }
     }
 }
