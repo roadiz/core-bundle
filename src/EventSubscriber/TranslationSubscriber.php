409930477f91798b787d167ac0fd9b610d1373be
Ambroise Maupate <ambroise@rezo-zero.com>
2022-05-24 16:47:16 +0200

    fix: TranslationSubscriber dispatch CachePurgeRequestEvent

--- src/EventSubscriber/TranslationSubscriber.php
+++ src/EventSubscriber/TranslationSubscriber.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace RZ\Roadiz\CoreBundle\EventSubscriber;
 
 use Doctrine\Common\Cache\CacheProvider;
+use RZ\Roadiz\CoreBundle\Event\Cache\CachePurgeRequestEvent;
 use RZ\Roadiz\CoreBundle\Event\Translation\TranslationCreatedEvent;
 use RZ\Roadiz\CoreBundle\Event\Translation\TranslationDeletedEvent;
 use RZ\Roadiz\CoreBundle\Event\Translation\TranslationUpdatedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Contracts\EventDispatcher\Event;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 /**
  * Subscribe to Translation event to clear result cache.
@@ -37,8 +40,9 @@ class TranslationSubscriber implements EventSubscriberInterface
     /**
      * Empty nodeSources Url cache
      */
-    public function purgeCache()
+    public function purgeCache(Event $event, string $eventName, EventDispatcherInterface $dispatcher)
     {
         $this->cacheProvider->deleteAll();
+        $dispatcher->dispatch(new CachePurgeRequestEvent());
     }
 }
