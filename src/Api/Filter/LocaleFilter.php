ca1ac631206dcb38cc7ee32f066a45c553c97839
Ambroise Maupate <ambroise@rezo-zero.com>
2022-06-22 11:18:45 +0200

    feat: Support non-available locales if previewing

--- src/Api/Filter/LocaleFilter.php
+++ src/Api/Filter/LocaleFilter.php
@@ -7,12 +7,31 @@ namespace RZ\Roadiz\CoreBundle\Api\Filter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use ApiPlatform\Core\Exception\InvalidArgumentException;
 use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
+use Psr\Log\LoggerInterface;
 use RZ\Roadiz\CoreBundle\Entity\NodesSources;
 use RZ\Roadiz\CoreBundle\Entity\Translation;
+use RZ\Roadiz\CoreBundle\Preview\PreviewResolverInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 final class LocaleFilter extends GeneratedEntityFilter
 {
     public const PROPERTY = '_locale';
+
+    private PreviewResolverInterface $previewResolver;
+
+    public function __construct(
+        PreviewResolverInterface $previewResolver,
+        ManagerRegistry $managerRegistry,
+        RequestStack $requestStack,
+        string $generatedEntityNamespacePattern = '#^App\\\GeneratedEntity\\\NS(?:[a-zA-Z]+)$#',
+        LoggerInterface $logger = null,
+        array $properties = null
+    ) {
+        parent::__construct($managerRegistry, $requestStack, $generatedEntityNamespacePattern, $logger, $properties);
+        $this->previewResolver = $previewResolver;
+    }
+
 
     /**
      * Passes a property through the filter.
@@ -33,37 +52,47 @@ final class LocaleFilter extends GeneratedEntityFilter
         string $resourceClass,
         string $operationName = null
     ) {
-        if ($property === self::PROPERTY) {
-            $supportedLocales = $this->managerRegistry->getRepository(Translation::class)->getAvailableLocales();
-            if (!in_array($value, $supportedLocales)) {
-                throw new \Exception(
-                    sprintf(
-                        'Locale filter value "%s" not supported. Supported values are %s',
-                        $value,
-                        implode(', ', $supportedLocales)
-                    )
-                );
+        if ($property !== self::PROPERTY) {
+            return;
+        }
+
+        if ($this->previewResolver->isPreview()) {
+            $supportedLocales = $this->managerRegistry
+                ->getRepository(Translation::class)
+                ->getAllLocales();
+        } else {
+            $supportedLocales = $this->managerRegistry
+                ->getRepository(Translation::class)
+                ->getAvailableLocales();
+        }
+        if (!in_array($value, $supportedLocales)) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    'Locale filter value "%s" not supported. Supported values are %s',
+                    $value,
+                    implode(', ', $supportedLocales)
+                )
+            );
+        }
+
+        /*
+         * Apply translation filter only for NodesSources
+         */
+        if (
+            $resourceClass === NodesSources::class ||
+            preg_match($this->getGeneratedEntityNamespacePattern(), $resourceClass) > 0
+        ) {
+            $translation = $this->managerRegistry
+                ->getRepository(Translation::class)
+                ->findOneAvailableByLocaleOrOverrideLocale($value);
+
+            if (null === $translation) {
+                throw new InvalidArgumentException('No translation exist for locale: ' . $value);
             }
 
-            /*
-             * Apply translation filter only for NodesSources
-             */
-            if (
-                $resourceClass === NodesSources::class ||
-                preg_match($this->getGeneratedEntityNamespacePattern(), $resourceClass) > 0
-            ) {
-                $translation = $this->managerRegistry
-                    ->getRepository(Translation::class)
-                    ->findOneAvailableByLocaleOrOverrideLocale($value);
-
-                if (null === $translation) {
-                    throw new InvalidArgumentException('No translation exist for locale: ' . $value);
-                }
-
-                $queryBuilder
-                    ->andWhere($queryBuilder->expr()->eq('o.translation', ':translation'))
-                    ->setParameter('translation', $translation);
-            }
+            $queryBuilder
+                ->andWhere($queryBuilder->expr()->eq('o.translation', ':translation'))
+                ->setParameter('translation', $translation);
         }
     }
 
