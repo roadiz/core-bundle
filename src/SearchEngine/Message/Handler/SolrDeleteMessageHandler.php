785c5593db7a0fa4a3b11e3d277a035ff63d2361
Ambroise Maupate <ambroise@rezo-zero.com>
2022-07-25 13:17:03 +0200

    fix(SearchEngine): Do no trigger error on Solr messages if Solr is not available

--- src/SearchEngine/Message/Handler/SolrDeleteMessageHandler.php
+++ src/SearchEngine/Message/Handler/SolrDeleteMessageHandler.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace RZ\Roadiz\CoreBundle\SearchEngine\Message\Handler;
 
 use Psr\Log\LoggerInterface;
+use RZ\Roadiz\CoreBundle\Exception\SolrServerNotAvailableException;
 use RZ\Roadiz\CoreBundle\SearchEngine\Indexer\IndexerFactoryInterface;
 use RZ\Roadiz\CoreBundle\SearchEngine\Message\SolrDeleteMessage;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
@@ -30,6 +31,8 @@ final class SolrDeleteMessageHandler implements MessageHandlerInterface
             if (!empty($message->getIdentifier())) {
                 $this->indexerFactory->getIndexerFor($message->getClassname())->delete($message->getIdentifier());
             }
+        } catch (SolrServerNotAvailableException $exception) {
+            return;
         } catch (\LogicException $exception) {
             $this->logger->error($exception->getMessage());
         }
