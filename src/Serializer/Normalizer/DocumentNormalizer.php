99dcb8877beeedd31ba7645558e599cf22372c62
Ambroise Maupate <ambroise@rezo-zero.com>
2022-10-03 15:08:42 +0200

    feat(Normalization): Set normalizer decorator priorities

--- src/Serializer/Normalizer/DocumentNormalizer.php
+++ src/Serializer/Normalizer/DocumentNormalizer.php
@@ -7,26 +7,12 @@ namespace RZ\Roadiz\CoreBundle\Serializer\Normalizer;
 use RZ\Roadiz\Core\AbstractEntities\TranslationInterface;
 use RZ\Roadiz\CoreBundle\Entity\Document;
 use RZ\Roadiz\CoreBundle\Entity\DocumentTranslation;
-use RZ\Roadiz\Document\DocumentFinderInterface;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 /**
  * Override Document default normalization.
  */
 final class DocumentNormalizer extends AbstractPathNormalizer
 {
-    protected DocumentFinderInterface $documentFinder;
-
-    public function __construct(
-        NormalizerInterface $decorated,
-        UrlGeneratorInterface $urlGenerator,
-        DocumentFinderInterface $documentFinder
-    ) {
-        parent::__construct($decorated, $urlGenerator);
-        $this->documentFinder = $documentFinder;
-    }
-
     /**
      * @param mixed $object
      * @param string|null $format
@@ -40,9 +26,6 @@ final class DocumentNormalizer extends AbstractPathNormalizer
         if ($object instanceof Document && is_array($data)) {
             $data['type'] = $object->getShortType();
 
-            /** @var array<string> $serializationGroups */
-            $serializationGroups = isset($context['groups']) && is_array($context['groups']) ? $context['groups'] : [];
-
             if (isset($context['translation']) && $context['translation'] instanceof TranslationInterface) {
                 $translatedData = $object->getDocumentTranslationsByTranslation($context['translation'])->first() ?: null;
                 if ($translatedData instanceof DocumentTranslation) {
@@ -51,30 +34,6 @@ final class DocumentNormalizer extends AbstractPathNormalizer
                     $data['copyright'] = $translatedData->getCopyright();
                     $data['alt'] = !empty($translatedData->getName()) ? $translatedData->getName() : $object->getFilename();
                     $data['externalUrl'] = $translatedData->getExternalUrl();
-                }
-            }
-
-            if (in_array('document_display_sources', $serializationGroups)) {
-                /*
-                 * Reduce serialization group to avoid normalization loop.
-                 */
-                $sourcesContext = $context;
-                $sourcesContext['groups'] = ['document_display'];
-
-                if ($object->isLocal() && $object->isVideo()) {
-                    $data['altSources'] = [];
-                    foreach ($this->documentFinder->findVideosWithFilename($object->getRelativePath()) as $document) {
-                        if ($document->getRelativePath() !== $object->getRelativePath()) {
-                            $data['altSources'][] = $this->decorated->normalize($document, $format, $sourcesContext);
-                        }
-                    }
-                } elseif ($object->isLocal() && $object->isAudio()) {
-                    $data['altSources'] = [];
-                    foreach ($this->documentFinder->findAudiosWithFilename($object->getRelativePath()) as $document) {
-                        if ($document->getRelativePath() !== $object->getRelativePath()) {
-                            $data['altSources'][] = $this->decorated->normalize($document, $format, $sourcesContext);
-                        }
-                    }
                 }
             }
         }
