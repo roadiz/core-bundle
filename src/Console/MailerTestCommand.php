2c6d89cfe5309532406a0e6f89f595138f67fbfe
Ambroise Maupate <ambroise@rezo-zero.com>
2022-10-14 17:37:45 +0200

    fix: Fixed src/Console/MailerTestCommand

--- src/Console/MailerTestCommand.php
+++ src/Console/MailerTestCommand.php
@@ -8,6 +8,7 @@ use RZ\Roadiz\CoreBundle\Mailer\EmailManager;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Mime\Address;
@@ -29,22 +30,29 @@ class MailerTestCommand extends Command
     protected function configure()
     {
         $this->setName('mailer:send:test')
-            ->addArgument('email', InputArgument::REQUIRED, 'Receiver email.')
+            ->addArgument('email', InputArgument::REQUIRED, 'Receiver email address.')
+            ->addOption('from', 'f', InputOption::VALUE_REQUIRED, 'Sender envelop email address.')
             ->setDescription('Send a test email.')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $title = '[test] Roadiz test email';
+        $to = Address::create($input->getArgument('email'));
+        $from = Address::create($input->getOption('from') ?? 'test@roadiz.io');
+
         $this->emailManager
-            ->setReceiver(Address::create($input->getArgument('email')))
+            ->setReceiver($to)
+            ->setSender($from)
             // Uses email_sender customizable setting
-            ->setSubject('Test email')
+            ->setSubject($title)
             ->setEmailPlainTextTemplate('@RoadizCore/email/base_email.txt.twig')
             ->setEmailTemplate('@RoadizCore/email/base_email.html.twig')
             ->setAssignation([
-                'content' => 'This is a test email',
-                'mailContact' => 'test@roadiz.io'
+                'title' => $title,
+                'content' => 'This is a test email send to *' . $to->getAddress() . '* from `mailer:send:test` CLI command.',
+                'mailContact' => $from->getAddress()
             ])
             ->send();
         (new SymfonyStyle($input, $output))->success('Email sent.');
