229c36a8cac2a8d0d14b507a1ed542979a9b0bb9
Ambroise Maupate <ambroise@rezo-zero.com>
2022-11-30 16:06:12 +0100

    feat: Added Document publicUrl for non-processable documents

--- src/Serializer/Normalizer/DocumentNormalizer.php
+++ src/Serializer/Normalizer/DocumentNormalizer.php
@@ -4,27 +4,54 @@ declare(strict_types=1);
 
 namespace RZ\Roadiz\CoreBundle\Serializer\Normalizer;
 
+use League\Flysystem\FilesystemOperator;
 use RZ\Roadiz\Core\AbstractEntities\TranslationInterface;
 use RZ\Roadiz\CoreBundle\Entity\Document;
 use RZ\Roadiz\CoreBundle\Entity\DocumentTranslation;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 /**
  * Override Document default normalization.
  */
 final class DocumentNormalizer extends AbstractPathNormalizer
 {
+    private FilesystemOperator $documentsStorage;
+
+    public function __construct(
+        FilesystemOperator $documentsStorage,
+        NormalizerInterface $decorated,
+        UrlGeneratorInterface $urlGenerator
+    ) {
+        parent::__construct($decorated, $urlGenerator);
+        $this->documentsStorage = $documentsStorage;
+    }
+
     /**
      * @param mixed $object
      * @param string|null $format
      * @param array $context
-     * @return array|\ArrayObject|bool|float|int|mixed|string|null
+     * @return array|\ArrayObject|bool|float|int|string|null
      * @throws \Symfony\Component\Serializer\Exception\ExceptionInterface
      */
     public function normalize($object, $format = null, array $context = [])
     {
         $data = $this->decorated->normalize($object, $format, $context);
-        if ($object instanceof Document && is_array($data)) {
+        if (
+            $object instanceof Document &&
+            is_array($data)
+        ) {
             $data['type'] = $object->getShortType();
+
+            if (
+                !$object->isPrivate() &&
+                !$object->isProcessable()
+            ) {
+                $mountPath = $object->getMountPath();
+                if (null !== $mountPath) {
+                    $data['publicUrl'] = $this->documentsStorage->publicUrl($mountPath);
+                }
+            }
 
             if (isset($context['translation']) && $context['translation'] instanceof TranslationInterface) {
                 $translatedData = $object->getDocumentTranslationsByTranslation($context['translation'])->first() ?: null;
