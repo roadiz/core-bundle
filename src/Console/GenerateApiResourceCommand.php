53f1d06ba4439e00f94c56e8b59f23ae1a0978f0
Ambroise Maupate <ambroise@rezo-zero.com>
2022-12-01 19:53:27 +0100

    feat: Moved api resource creation from command to a dedicated service

--- src/Console/GenerateApiResourceCommand.php
+++ src/Console/GenerateApiResourceCommand.php
@@ -5,34 +5,26 @@ declare(strict_types=1);
 namespace RZ\Roadiz\CoreBundle\Console;
 
 use Doctrine\Persistence\ManagerRegistry;
-use LogicException;
-use RZ\Roadiz\Contracts\NodeType\NodeTypeInterface;
 use RZ\Roadiz\CoreBundle\Entity\NodeType;
+use RZ\Roadiz\CoreBundle\NodeType\ApiResourceGenerator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\String\UnicodeString;
-use Symfony\Component\Yaml\Yaml;
 
 class GenerateApiResourceCommand extends Command
 {
     protected ManagerRegistry $managerRegistry;
-    protected ParameterBagInterface $parameterBag;
+    protected ApiResourceGenerator $apiResourceGenerator;
 
-    /**
-     * @param ManagerRegistry $managerRegistry
-     * @param ParameterBagInterface $parameterBag
-     */
-    public function __construct(ManagerRegistry $managerRegistry, ParameterBagInterface $parameterBag)
-    {
+    public function __construct(
+        ManagerRegistry $managerRegistry,
+        ApiResourceGenerator $apiResourceGenerator
+    ) {
         parent::__construct();
         $this->managerRegistry = $managerRegistry;
-        $this->parameterBag = $parameterBag;
+        $this->apiResourceGenerator = $apiResourceGenerator;
     }
-
 
     protected function configure(): void
     {
@@ -49,37 +41,12 @@ class GenerateApiResourceCommand extends Command
         $nodeTypes = $this->managerRegistry
             ->getRepository(NodeType::class)
             ->findAll();
-        $filesystem = new Filesystem();
-        $projectDir = $this->parameterBag->get('kernel.project_dir');
-
-        if (!is_string($projectDir)) {
-            throw new LogicException('kernel.project_dir parameter does not exist.');
-        }
-        if (!$filesystem->exists($projectDir)) {
-            throw new LogicException($projectDir . ' folder does not exist.');
-        }
-
-        $directory = $projectDir . '/config/api_resources';
-        if (!$filesystem->exists($directory)) {
-            throw new LogicException($directory . ' folder does not exist.');
-        }
 
         if (count($nodeTypes) > 0) {
             foreach ($nodeTypes as $nt) {
-                $resourcePath = $directory . '/' . (new UnicodeString($nt->getName()))
-                        ->lower()
-                        ->prepend('ns')
-                        ->append('.yml')
-                        ->toString();
-                if (!$filesystem->exists($resourcePath)) {
-                    $filesystem->dumpFile(
-                        $resourcePath,
-                        Yaml::dump($this->getApiResourceDefinition($nt), 6)
-                    );
+                $resourcePath = $this->apiResourceGenerator->generate($nt);
+                if (null !== $resourcePath) {
                     $io->writeln("* API resource <info>" . $resourcePath . "</info> has been generated.");
-                    \clearstatcache(true, $resourcePath);
-                } else {
-                    $io->writeln("* API resource <info>" . $resourcePath . "</info> already exists, left untouched.");
                 }
             }
             return 0;
@@ -87,107 +54,5 @@ class GenerateApiResourceCommand extends Command
             $io->error('No available node-types…');
             return 1;
         }
-    }
-
-    protected function getApiResourceDefinition(NodeTypeInterface $nodeType): array
-    {
-        $fqcn = (new UnicodeString($nodeType->getSourceEntityFullQualifiedClassName()))
-            ->trimStart('\\')
-            ->toString();
-
-        return [ $fqcn => [
-            'iri' => $nodeType->getName(),
-            'shortName' => $nodeType->getName(),
-            'collectionOperations' => $this->getCollectionOperations($nodeType),
-            'itemOperations' => $this->getItemOperations($nodeType),
-        ]];
-    }
-
-    protected function getCollectionOperations(NodeTypeInterface $nodeType): array
-    {
-        if ($nodeType->isReachable()) {
-            $groups = [
-                "nodes_sources_base",
-                "nodes_sources_default",
-                "urls",
-                "tag_base",
-                "translation_base",
-                "document_display",
-                "document_thumbnails",
-                "document_display_sources",
-                ...$this->getGroupedFieldsSerializationGroups($nodeType)
-            ];
-            return [
-                'get' => [
-                    'method' => 'GET',
-                    'normalization_context' => [
-                        'enable_max_depth' => true,
-                        'groups' => array_values(array_filter(array_unique($groups)))
-                    ],
-                ]
-            ];
-        }
-        return [];
-    }
-
-    protected function getItemOperations(NodeTypeInterface $nodeType): array
-    {
-        $groups = [
-            "nodes_sources",
-            "urls",
-            "tag_base",
-            "translation_base",
-            "document_display",
-            "document_thumbnails",
-            "document_display_sources",
-            ...$this->getGroupedFieldsSerializationGroups($nodeType)
-        ];
-        $operations = [
-            'get' => [
-                'method' => 'GET',
-                'normalization_context' => [
-                    'groups' => array_values(array_filter(array_unique($groups)))
-                ],
-            ]
-        ];
-
-        /*
-         * Create itemOperation for WebResponseController action
-         */
-        if ($nodeType->isReachable()) {
-            $operations['getByPath'] = [
-                'method' => 'GET',
-                'normalization_context' => [
-                    'enable_max_depth' => true,
-                    'groups' => array_merge(array_values(array_filter(array_unique($groups))), [
-                        'web_response',
-                        'walker',
-                        'walker_level',
-                        'walker_metadata',
-                        'meta',
-                        'children',
-                        'children_count',
-                    ])
-                ],
-            ];
-        }
-
-        return $operations;
-    }
-
-    protected function getGroupedFieldsSerializationGroups(NodeTypeInterface $nodeType): array
-    {
-        $groups = [];
-        foreach ($nodeType->getFields() as $field) {
-            if (null !== $field->getGroupNameCanonical()) {
-                $groups[] = (new UnicodeString($field->getGroupNameCanonical()))
-                    ->lower()
-                    ->snake()
-                    ->prepend('nodes_sources_')
-                    ->toString()
-                ;
-            }
-        }
-        return $groups;
     }
 }
---
+++ src/NodeType/ApiResourceGenerator.php
@@ -0,0 +1,152 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RZ\Roadiz\CoreBundle\NodeType;
+
+use LogicException;
+use RZ\Roadiz\Contracts\NodeType\NodeTypeInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\String\UnicodeString;
+use Symfony\Component\Yaml\Yaml;
+
+final class ApiResourceGenerator
+{
+    private string $apiResourcesDir;
+
+    public function __construct(string $apiResourcesDir)
+    {
+        $this->apiResourcesDir = $apiResourcesDir;
+    }
+
+    /**
+     * @param NodeTypeInterface $nodeType
+     * @return string|null Generated resource file path or null if nothing done.
+     */
+    public function generate(NodeTypeInterface $nodeType): ?string
+    {
+        $filesystem = new Filesystem();
+
+        if (!$filesystem->exists($this->apiResourcesDir)) {
+            throw new LogicException($this->apiResourcesDir . ' folder does not exist.');
+        }
+
+        $resourcePath = $this->apiResourcesDir . '/' . (new UnicodeString($nodeType->getName()))
+                ->lower()
+                ->prepend('ns')
+                ->append('.yml')
+                ->toString();
+        if (!$filesystem->exists($resourcePath)) {
+            $filesystem->dumpFile(
+                $resourcePath,
+                Yaml::dump($this->getApiResourceDefinition($nodeType), 6)
+            );
+            \clearstatcache(true, $resourcePath);
+            return $resourcePath;
+        } else {
+            return null;
+        }
+    }
+
+    protected function getApiResourceDefinition(NodeTypeInterface $nodeType): array
+    {
+        $fqcn = (new UnicodeString($nodeType->getSourceEntityFullQualifiedClassName()))
+            ->trimStart('\\')
+            ->toString();
+
+        return [ $fqcn => [
+            'iri' => $nodeType->getName(),
+            'shortName' => $nodeType->getName(),
+            'collectionOperations' => $this->getCollectionOperations($nodeType),
+            'itemOperations' => $this->getItemOperations($nodeType),
+        ]];
+    }
+
+    protected function getCollectionOperations(NodeTypeInterface $nodeType): array
+    {
+        if ($nodeType->isReachable()) {
+            $groups = [
+                "nodes_sources_base",
+                "nodes_sources_default",
+                "urls",
+                "tag_base",
+                "translation_base",
+                "document_display",
+                "document_thumbnails",
+                "document_display_sources",
+                ...$this->getGroupedFieldsSerializationGroups($nodeType)
+            ];
+            return [
+                'get' => [
+                    'method' => 'GET',
+                    'normalization_context' => [
+                        'enable_max_depth' => true,
+                        'groups' => array_values(array_filter(array_unique($groups)))
+                    ],
+                ]
+            ];
+        }
+        return [];
+    }
+
+    protected function getItemOperations(NodeTypeInterface $nodeType): array
+    {
+        $groups = [
+            "nodes_sources",
+            "urls",
+            "tag_base",
+            "translation_base",
+            "document_display",
+            "document_thumbnails",
+            "document_display_sources",
+            ...$this->getGroupedFieldsSerializationGroups($nodeType)
+        ];
+        $operations = [
+            'get' => [
+                'method' => 'GET',
+                'normalization_context' => [
+                    'groups' => array_values(array_filter(array_unique($groups)))
+                ],
+            ]
+        ];
+
+        /*
+         * Create itemOperation for WebResponseController action
+         */
+        if ($nodeType->isReachable()) {
+            $operations['getByPath'] = [
+                'method' => 'GET',
+                'normalization_context' => [
+                    'enable_max_depth' => true,
+                    'groups' => array_merge(array_values(array_filter(array_unique($groups))), [
+                        'web_response',
+                        'walker',
+                        'walker_level',
+                        'walker_metadata',
+                        'meta',
+                        'children',
+                        'children_count',
+                    ])
+                ],
+            ];
+        }
+
+        return $operations;
+    }
+
+    protected function getGroupedFieldsSerializationGroups(NodeTypeInterface $nodeType): array
+    {
+        $groups = [];
+        foreach ($nodeType->getFields() as $field) {
+            if (null !== $field->getGroupNameCanonical()) {
+                $groups[] = (new UnicodeString($field->getGroupNameCanonical()))
+                    ->lower()
+                    ->snake()
+                    ->prepend('nodes_sources_')
+                    ->toString()
+                ;
+            }
+        }
+        return $groups;
+    }
+}
