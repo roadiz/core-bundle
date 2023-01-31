41fa26c512788212ddb2d62319dfbd56ff898884
Ambroise Maupate <ambroise@rezo-zero.com>
2023-01-31 14:29:31 +0100

    feat: Added Setting events for CRUD operations

---
+++ src/Event/Setting/SettingCreatedEvent.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RZ\Roadiz\CoreBundle\Event\Setting;
+
+use RZ\Roadiz\CoreBundle\Event\FilterSettingEvent;
+
+final class SettingCreatedEvent extends FilterSettingEvent
+{
+}
