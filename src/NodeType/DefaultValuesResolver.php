ba1fb0844758f0f9467be7fa5a84932a5aa7efd1
roadiz-ci <ambroise+ci@roadiz.io>
2023-03-23 17:39:14 +0000

    fix(EntityGenerator): Nullable `$field->getDefaultValues()`

--- src/NodeType/DefaultValuesResolver.php
+++ src/NodeType/DefaultValuesResolver.php
@@ -31,7 +31,7 @@ final class DefaultValuesResolver implements DefaultValuesResolverInterface
          * SQL field won't be shared between all node types.
          */
         if ($this->inheritanceType === Configuration::INHERITANCE_TYPE_JOINED) {
-            return array_map('trim', explode(',', $field->getDefaultValues()));
+            return array_map('trim', explode(',', $field->getDefaultValues() ?? ''));
         } else {
             /*
              * With single table inheritance, we need to get all default values
@@ -43,7 +43,7 @@ final class DefaultValuesResolver implements DefaultValuesResolverInterface
                 'type' => $field->getType(),
             ]);
             foreach ($nodeTypeFields as $nodeTypeField) {
-                $defaultValues = array_merge($defaultValues, array_map('trim', explode(',', $nodeTypeField->getDefaultValues())));
+                $defaultValues = array_merge($defaultValues, array_map('trim', explode(',', $nodeTypeField->getDefaultValues() ?? '')));
             }
             return $defaultValues;
         }
