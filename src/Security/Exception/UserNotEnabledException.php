73a75e1272bca9421980ae43e7404d448e73f0bb
roadiz-ci <ambroise+ci@roadiz.io>
2023-08-03 13:39:19 +0000

    Merge branch hotfix/v2.1.37

---
+++ src/Security/Exception/UserNotEnabledException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RZ\Roadiz\CoreBundle\Security\Exception;
+
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+
+final class UserNotEnabledException extends AuthenticationException
+{
+    public function getMessageKey(): string
+    {
+        return 'Your user account is not enabled. Contact an administrator.';
+    }
+}
