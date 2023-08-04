8f4b177865cb66d109fc4267a2f6aacb08d5d344
roadiz-ci <ambroise+ci@roadiz.io>
2023-08-04 15:49:16 +0000

    Merge tag v2.1.40 into develop

--- src/Api/TreeWalker/TreeWalkerGenerator.php
+++ src/Api/TreeWalker/TreeWalkerGenerator.php
@@ -6,9 +6,12 @@ namespace RZ\Roadiz\CoreBundle\Api\TreeWalker;
 
 use Psr\Cache\CacheItemPoolInterface;
 use RZ\Roadiz\Core\AbstractEntities\TranslationInterface;
+use RZ\Roadiz\CoreBundle\Api\TreeWalker\Definition\DefinitionFactoryConfiguration;
+use RZ\Roadiz\CoreBundle\Api\TreeWalker\Definition\DefinitionFactoryInterface;
 use RZ\Roadiz\CoreBundle\Bag\NodeTypes;
 use RZ\Roadiz\CoreBundle\Entity\NodesSources;
 use RZ\Roadiz\CoreBundle\EntityApi\NodeSourceApi;
+use RZ\TreeWalker\AbstractWalker;
 use RZ\TreeWalker\WalkerContextInterface;
 use RZ\TreeWalker\WalkerInterface;
 use Symfony\Component\String\UnicodeString;
@@ -19,6 +22,11 @@ final class TreeWalkerGenerator
     private NodeTypes $nodeTypesBag;
     private WalkerContextInterface $walkerContext;
     private CacheItemPoolInterface $cacheItemPool;
+
+    /**
+     * @var array<class-string, DefinitionFactoryConfiguration>
+     */
+    private array $walkerDefinitionFactories = [];
 
     public function __construct(
         NodeSourceApi $nodeSourceApi,
@@ -34,7 +42,7 @@ final class TreeWalkerGenerator
 
     /**
      * @param string $nodeType
-     * @param class-string $walkerClass
+     * @param class-string<AbstractWalker> $walkerClass
      * @param TranslationInterface $translation
      * @param int $maxLevel
      * @return array<string, WalkerInterface>
@@ -59,11 +67,9 @@ final class TreeWalkerGenerator
                 ->camel()
                 ->toString();
 
-            /** @var callable $callable */
-            $callable = [$walkerClass, 'build'];
-            $walkers[$walkerName] = call_user_func(
-                $callable,
+            $walkers[$walkerName] = $this->buildForRoot(
                 $root,
+                $walkerClass,
                 $this->walkerContext,
                 $maxLevel,
                 $this->cacheItemPool
@@ -71,5 +77,63 @@ final class TreeWalkerGenerator
         }
 
         return $walkers;
+    }
+
+    /**
+     * @param object $root
+     * @param class-string<AbstractWalker> $walkerClass
+     * @param WalkerContextInterface $walkerContext
+     * @param int $maxLevel
+     * @param CacheItemPoolInterface $cacheItemPool
+     * @return WalkerInterface
+     */
+    public function buildForRoot(
+        object $root,
+        string $walkerClass,
+        WalkerContextInterface $walkerContext,
+        int $maxLevel,
+        CacheItemPoolInterface $cacheItemPool
+    ): WalkerInterface {
+        /** @var callable $callable */
+        $callable = [$walkerClass, 'build'];
+        $walker = call_user_func(
+            $callable,
+            $root,
+            $walkerContext,
+            $maxLevel,
+            $cacheItemPool
+        );
+
+        foreach ($this->walkerDefinitionFactories as $definitionFactoryConfiguration) {
+            $walker->addDefinition(
+                $definitionFactoryConfiguration->classname,
+                $definitionFactoryConfiguration->definitionFactory->create(
+                    $this->walkerContext,
+                    $definitionFactoryConfiguration->onlyVisible
+                )
+            );
+        }
+        return $walker;
+    }
+
+    /**
+     * Inject definition from factories registered in the container
+     * using `roadiz_core.tree_walker_definition_factory` tag.
+     *
+     * @param class-string $classname
+     * @param DefinitionFactoryInterface $definitionFactory
+     * @param bool $onlyVisible
+     * @return void
+     */
+    public function addDefinitionFactoryConfiguration(
+        string $classname,
+        DefinitionFactoryInterface $definitionFactory,
+        bool $onlyVisible
+    ): void {
+        $this->walkerDefinitionFactories[$classname] = new DefinitionFactoryConfiguration(
+            $classname,
+            $definitionFactory,
+            $onlyVisible
+        );
     }
 }
