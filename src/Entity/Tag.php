0c82f1532b4c478fd57fd145f0f4b7aaa103e415
roadiz-ci <ambroise+ci@roadiz.io>
2023-09-07 14:08:51 +0000

    Merge branch hotfix/v2.1.44

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -160,8 +160,12 @@ class Tag extends AbstractDateTimedPositioned implements LeafInterface
     #[ApiFilter(BaseFilter\SearchFilter::class, properties: [
         "nodesTags.node" => "exact",
         "nodesTags.node.nodeName" => "exact",
+        "nodesTags.node.parent" => "exact",
+        "nodesTags.node.parent.nodeName" => "exact",
         "nodesTags.node.nodeType" => "exact",
         "nodesTags.node.nodeType.name" => "exact",
+        "nodesTags.node.nodesTags.tag" => "exact",
+        "nodesTags.node.nodesTags.tag.tagName" => "exact",
     ])]
     #[ApiFilter(BaseFilter\BooleanFilter::class, properties: [
         "nodesTags.node.visible",
