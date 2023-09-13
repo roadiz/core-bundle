2721e96b97c07ae9fc67f266e09fb4c52cf410f0
roadiz-ci <ambroise+ci@roadiz.io>
2023-09-13 15:24:28 +0000

    Merge branch hotfix/v2.1.45

--- src/ListManager/AbstractEntityListManager.php
+++ src/ListManager/AbstractEntityListManager.php
@@ -213,4 +213,12 @@ abstract class AbstractEntityListManager implements EntityListManagerInterface
     {
         return (int) ceil($this->getItemCount() / $this->getItemPerPage());
     }
+
+    protected function validateOrderingFieldName(string $field): void
+    {
+        // check if field is a valid name without any SQL injection
+        if (\preg_match('/^[a-zA-Z0-9_.]+$/', $field) !== 1) {
+            throw new \InvalidArgumentException('Field name is not valid.');
+        }
+    }
 }
