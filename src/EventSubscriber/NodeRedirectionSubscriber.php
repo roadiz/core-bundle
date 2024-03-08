ca7dbc242a3e51e739eca3948cdf5c676b4133fd
roadiz-ci <ambroise+ci@roadiz.io>
2024-03-08 22:18:19 +0000

    refactor: Some constructor args refactoring, remove direct dependency on KernelInterface

--- src/EventSubscriber/NodeRedirectionSubscriber.php
+++ src/EventSubscriber/NodeRedirectionSubscriber.php
@@ -10,30 +10,17 @@ use RZ\Roadiz\CoreBundle\Node\NodeMover;
 use RZ\Roadiz\CoreBundle\Preview\PreviewResolverInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\KernelInterface;
 
 /**
  * Subscribe to Node, NodesSources and UrlAlias event to clear ns url cache.
  */
 class NodeRedirectionSubscriber implements EventSubscriberInterface
 {
-    protected NodeMover $nodeMover;
-    protected KernelInterface $kernel;
-    protected PreviewResolverInterface $previewResolver;
-
-    /**
-     * @param NodeMover $nodeMover
-     * @param KernelInterface $kernel
-     * @param PreviewResolverInterface $previewResolver
-     */
     public function __construct(
-        NodeMover $nodeMover,
-        KernelInterface $kernel,
-        PreviewResolverInterface $previewResolver
+        protected readonly NodeMover $nodeMover,
+        protected readonly string $kernelEnvironment,
+        protected readonly PreviewResolverInterface $previewResolver
     ) {
-        $this->nodeMover = $nodeMover;
-        $this->kernel = $kernel;
-        $this->previewResolver = $previewResolver;
     }
 
     public static function getSubscribedEvents(): array
@@ -56,7 +43,7 @@ class NodeRedirectionSubscriber implements EventSubscriberInterface
         EventDispatcherInterface $dispatcher
     ): void {
         if (
-            $this->kernel->getEnvironment() === 'prod' &&
+            $this->kernelEnvironment === 'prod' &&
             !$this->previewResolver->isPreview() &&
             null !== $event->getNode() &&
             $event->getNode()->isPublished() &&
