a13935d0e27c31fefc87f87aab17fa7b50be3c3d
roadiz-ci <ambroise+ci@roadiz.io>
2025-03-23 23:15:02 +0000

    Merge branch hotfix/v2.5.2

--- deprecated.php
+++ deprecated.php
@@ -4,4 +4,6 @@ declare(strict_types=1);
 
 use RZ\Roadiz\CoreBundle\Entity\AbstractField;
 
-\class_alias(AbstractField::class, RZ\Roadiz\Core\AbstractEntities\AbstractField::class);
+if (!class_exists(RZ\Roadiz\Core\AbstractEntities\AbstractField::class)) {
+    \class_alias(AbstractField::class, RZ\Roadiz\Core\AbstractEntities\AbstractField::class);
+}
