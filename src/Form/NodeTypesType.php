1b60741c114e7e7775b7b0226a82a6dc11b496fc
roadiz-ci <ambroise+ci@roadiz.io>
2025-04-17 09:00:26 +0000

    Merge branch hotfix/v2.5.8

--- src/Form/NodeTypesType.php
+++ src/Form/NodeTypesType.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace RZ\Roadiz\CoreBundle\Form;
 
 use RZ\Roadiz\CoreBundle\Bag\DecoratedNodeTypes;
+use RZ\Roadiz\CoreBundle\Entity\NodeType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\OptionsResolver\Options;
@@ -20,22 +21,55 @@ final class NodeTypesType extends AbstractType
     {
         $resolver->setDefaults([
             'showInvisible' => false,
+            'currentType' => null,
+            // Hard-code the most used node-type here
+            'preferred_choices' => ['Page'],
         ]);
         $resolver->setAllowedTypes('showInvisible', ['boolean']);
+        $resolver->setAllowedTypes('currentType', ['null', NodeType::class]);
         $resolver->setNormalizer('choices', function (Options $options, $choices) {
-            if (true === $options['showInvisible']) {
-                $nodeTypes = $this->nodeTypesBag->all();
-            } else {
-                $nodeTypes = $this->nodeTypesBag->allVisible();
-            }
+            $nodeTypes = $this->getNodeTypes($options);
 
             foreach ($nodeTypes as $nodeType) {
+                if (null !== $options['currentType'] && $options['currentType']->getName() === $nodeType->getName()) {
+                    continue;
+                }
                 $choices[$nodeType->getDisplayName()] = $nodeType->getName();
             }
             ksort($choices);
 
             return $choices;
         });
+        $resolver->setNormalizer('group_by', function (Options $options) {
+            return function ($choice, $key, $value) use ($options) {
+                $nodeTypes = $this->getNodeTypes($options);
+
+                foreach ($nodeTypes as $nodeType) {
+                    if ($value !== $nodeType->getName()) {
+                        continue;
+                    }
+                    if ($nodeType->isReachable()) {
+                        return 'reachable';
+                    } else {
+                        return 'not_reachable';
+                    }
+                }
+
+                return null;
+            };
+        });
+    }
+
+    /**
+     * @return array<NodeType>
+     */
+    private function getNodeTypes(Options $options): array
+    {
+        if (true === $options['showInvisible']) {
+            return $this->nodeTypesBag->all();
+        }
+
+        return $this->nodeTypesBag->allVisible();
     }
 
     public function getParent(): ?string
