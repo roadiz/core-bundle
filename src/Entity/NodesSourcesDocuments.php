8a934db13ea68c458ba5c3b3492c64fc8e21ad55
roadiz-ci <ambroise+ci@roadiz.io>
2025-05-28 09:15:53 +0000

    Merge tag v2.5.17 into develop

--- src/Entity/NodesSourcesDocuments.php
+++ src/Entity/NodesSourcesDocuments.php
@@ -80,16 +80,9 @@ class NodesSourcesDocuments extends AbstractPositioned
 
     /**
      * Create a new relation between NodeSource, a Document and a NodeTypeField.
-     *
-     * @param NodesSources                $nodeSource NodesSources and inherited types
-     * @param Document                    $document   Document to link
-     * @param NodeTypeFieldInterface|null $field      NodeTypeField
      */
     public function __construct(NodesSources $nodeSource, Document $document, ?NodeTypeFieldInterface $field = null)
     {
-        if (!$field instanceof NodeTypeField) {
-            throw new \InvalidArgumentException('NodesSourcesDocuments field must be a NodeTypeField instance.');
-        }
         $this->nodeSource = $nodeSource;
         $this->document = $document;
         $this->initializeFieldAwareEntityTrait($field);
