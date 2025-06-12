04878e66874c793a73eecf787bccf08eebab75b2
roadiz-ci <ambroise+ci@roadiz.io>
2025-06-12 11:21:43 +0000

    fix(Migration): Remove unused indexes from custom_forms, redirections, and webhooks tables

--- migrations/Version20250603212716.php
+++ migrations/Version20250603212716.php
@@ -14,7 +14,7 @@ final class Version20250603212716 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Remove unused indexes on custom_forms, fonts, positioned_page_user, redirections and webhooks tables.';
+        return 'Remove unused indexes on custom_forms, redirections and webhooks tables.';
     }
 
     public function up(Schema $schema): void
@@ -24,15 +24,6 @@ final class Version20250603212716 extends AbstractMigration
         SQL);
         $this->addSql(<<<'SQL'
             DROP INDEX IDX_3E32E39E43625D9F ON custom_forms
-        SQL);
-        $this->addSql(<<<'SQL'
-            DROP INDEX IDX_7303E8FB8B8E8428 ON fonts
-        SQL);
-        $this->addSql(<<<'SQL'
-            DROP INDEX IDX_7303E8FB43625D9F ON fonts
-        SQL);
-        $this->addSql(<<<'SQL'
-            DROP INDEX IDX_3C43EBC7462CE4F5 ON positioned_page_user
         SQL);
         $this->addSql(<<<'SQL'
             DROP INDEX IDX_38F5ECE48B8E8428 ON redirections
@@ -55,15 +46,6 @@ final class Version20250603212716 extends AbstractMigration
         SQL);
         $this->addSql(<<<'SQL'
             CREATE INDEX IDX_3E32E39E43625D9F ON custom_forms (updated_at)
-        SQL);
-        $this->addSql(<<<'SQL'
-            CREATE INDEX IDX_7303E8FB8B8E8428 ON fonts (created_at)
-        SQL);
-        $this->addSql(<<<'SQL'
-            CREATE INDEX IDX_7303E8FB43625D9F ON fonts (updated_at)
-        SQL);
-        $this->addSql(<<<'SQL'
-            CREATE INDEX IDX_3C43EBC7462CE4F5 ON positioned_page_user (position)
         SQL);
         $this->addSql(<<<'SQL'
             CREATE INDEX IDX_38F5ECE48B8E8428 ON redirections (created_at)
